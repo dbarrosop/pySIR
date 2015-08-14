3c3e57ba5097b16c7c03f833537d3eb3b9f62083
David Barroso <dbarrosop@dravetech.com>
2015-08-14 12:39:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import uuid
 from setuptools import setup
 from pip.req import parse_requirements
 
-version = '0.42'
+version = '0.43'
 
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
 reqs = [str(ir.req) for ir in install_reqs]
